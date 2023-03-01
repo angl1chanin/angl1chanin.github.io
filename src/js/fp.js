7ccab34485c181373de3ffd47ff9502e0a214cef
slicetopieces <markovich1990mark@gmail.com>
2023-03-01 20:20:29 +0200

    Modified: made header sticky

--- src/js/fp.js
+++ src/js/fp.js
@@ -4,7 +4,6 @@ new fullpage('#fullpage', {
     animateAnchor: true,
     slidesNavigation: true,
     navigation: true,
-    sectionsColors: ['#000', '#fff', '#fff', '#fff', '#F6F7FF'],
     anchors: ['home', 'about', 'portfolio', 'skills', 'contact'],
     onLeave: function (section, origin) {
         /* 
@@ -18,14 +17,14 @@ new fullpage('#fullpage', {
                 burgerStatus.checked = false;
             }
 
-            let returnBtn = document.querySelector('.return');
+            let returnBtn = document.querySelector('.return-button');
             returnBtn.style.bottom = '3rem';
         } 
         /*
             Code below hide return button if section is already 'home'
         */
         else if (origin.index === 0) {
-            let returnBtn = document.querySelector('.return');
+            let returnBtn = document.querySelector('.return-button');
             returnBtn.style.bottom = '-10rem';
         }
         
@@ -36,7 +35,7 @@ new fullpage('#fullpage', {
     Here the realize of work of return button.
     It will move to 'home' section on click
 */
-let returnBtn = document.querySelector('.return');
+let returnBtn = document.querySelector('.return-button');
 returnBtn.addEventListener('click', () => {
     fullpage_api.moveTo('home', 1);
 })
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,12 +1,13 @@
+const BODY_CLASS = 'dark';
 const ACTIVE_THEME = localStorage.getItem('theme');
 let switcher = document.getElementById('theme__checkbox');
 
 function switchTheme(theme) {
     if (theme === 'dark') {
-        document.body.classList.add('dark');
+        document.body.classList.add(BODY_CLASS);
         localStorage.setItem('theme', theme);
     } else if (theme === 'light') {
-        document.body.classList.remove('dark');
+        document.body.classList.remove(BODY_CLASS);
         localStorage.setItem('theme', theme);
     }
 }
@@ -15,9 +16,11 @@ switcher.addEventListener('click', () => {
     switcher.checked ? switchTheme('dark') : switchTheme('light');
 });
 
-if (ACTIVE_THEME === null) {
-    switchTheme('light');
-    switcher.click();
-} else {
-    switchTheme(ACTIVE_THEME);
+window.onload = function () {
+    if (ACTIVE_THEME === null) {
+        switchTheme('light');
+    } else if (ACTIVE_THEME === 'dark') {
+        switchTheme('dark');
+        switcher.checked = true;
+    }
 }
